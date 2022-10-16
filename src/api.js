2c30159ca2a6c757855d5bc224bf9ec748057a37
Niwantha <chamarawix@gmail.com>
2022-10-16 16:58:53 +0900

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoAPIOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '482c2d0055msh8c975cbfcfa2371p11665ejsn877f3b3c290d',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "00a33b43ccca270ebdef327e163a2d67"
